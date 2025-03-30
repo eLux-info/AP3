a6d617e5590e0517fc3f1234609a8756e61d67c0
eLux-info <enzoluxinfo@gmail.com>
2025-03-30 22:35:59 +0200

    Update WindowsAuth.java

--- src/com/arrasgame/auth/WindowsAuth.java
+++ src/com/arrasgame/auth/WindowsAuth.java
@@ -6,7 +6,6 @@ import com.sun.jna.platform.win32.Win32Exception;
 public class WindowsAuth {
     public static boolean authenticate(String username, String password) {
         try {
-            // Nouvelle méthode pour JNA 5.12+
             return Advapi32.INSTANCE.LogonUser(
                 username,
                 null, // Domaine (null pour local)
@@ -17,7 +16,6 @@ public class WindowsAuth {
             );
             return true;
         } catch (Win32Exception e) {
-            // Échec d'authentification
             return false;
         }
     }
